3c64b872cae1dfcb8a95b1aa99db877b995d57e9
nampd <nampd@hpt.vn>
2022-11-30 14:26:37 +0700

    Source base react router dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 import { Home, Publics, Login } from './containers/public'
 import { Routes, Route } from 'react-router-dom'
-
+import path from './ultis/path'
 function App() {
 
   return (
     <div className="App">
       <Routes>
-        <Route path='/*' element={<Publics />}>
-          <Route path='home' element={<Home />} />
-          <Route path='login' element={<Login />} />
+        <Route path={path.PUBLIC} element={<Publics />}>
+          <Route path={path.HOME} element={<Home />} />
+          <Route path={path.LOGIN} element={<Login />} />
         </Route>
       </Routes>
     </div>
