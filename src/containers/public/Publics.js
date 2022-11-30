65252caf1e82fe89b220b1d77af3ff81f41fd992
nampd <nampd@hpt.vn>
2022-11-30 15:10:36 +0700

    Source base Reactjs for any project

--- src/containers/public/Publics.js
+++ src/containers/public/Publics.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { SidebarLeft, SidebarRight } from '../../components'
 const Publics = () => {
   return (
-    <div>
-      Publics
-      <Outlet />
+    <div className='w-full flex'>
+      <div className='w-[240px] flex-none border border-blue-500'>
+        <SidebarLeft></SidebarLeft>
+      </div>
+      <div className=''>
+        <Outlet />
+      </div>
+      <div className='w-[329px]'><SidebarRight></SidebarRight></div>
     </div>
   )
 }
