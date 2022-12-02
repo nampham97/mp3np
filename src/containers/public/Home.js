cc3fa7c89eaa16fa2efd3f018a96e454e1aec6a9
nampd <nampd@hpt.vn>
2022-12-02 17:17:48 +0700

    Setup axios & api be

--- src/containers/public/Home.js
+++ src/containers/public/Home.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Header } from '../../components'
+import * as api from '../../api'
+
 const home = () => {
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        const fetchDataHome = async () => {
+            const res = await api.getHome()
+            console.log('data', res)
+        }
+
+        fetchDataHome()
+    }, [])
+
     return (
         <div className='overflow-y-auto'>
             <div className='h-[70px] px-[59px] flex items-center'>
